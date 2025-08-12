fe30e5e69bbc03c024875d6c9e5208ffac36a314
Angelo Pariz <parizsabrina@gmail.com>
2025-08-12 08:57:55 -0300

    aula1

---
+++ aula1_revisao.py/aula1_revisao.py
@@ -0,0 +1,10 @@
+x = "sabrina" 
+y = 0 
+z = 1.1 
+lista = [x,y,z]
+dicionario = {"nome": x}
+#importanto excel para dentro do phyton
+import pandas as pd
+df = pd.read_excel("C:\Users\angel\OneDrive\Documentos\analise_de_dados\livros_classics (1).xlsx")
+df
+             

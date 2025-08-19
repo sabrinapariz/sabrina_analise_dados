ab4b7aaccda160f7f50957179516767eecb537c3
Angelo Pariz <parizsabrina@gmail.com>
2025-08-19 09:07:29 -0300

    analisededadoslista.py

--- analisededadoslista.py
+++ analisededadoslista.py
@@ -6,57 +6,41 @@ df = pd.read_csv("C:/Users/angel/OneDrive/Documentos/analise_de_dados/imoveis_br
 df.head()
 df.tail()
 df.sample(5)
-
 #número de linhas e colunas
 df.shape
-
 #listar nomes das colunas
 df.columns
-
 #tipos de dados
 df.dtypes
-
 #estatísticas numéricas
 df.describe()
-
 #informações gerais
 df.info()
-
 #tipos de imóveis
 df['Tipo_Imovel'].unique()
-
 #filtrar imóveis acima de R$ 1.000.000,00
 df_caros = df["Valor_Imovel"] > 1000000
 df_1M = df.loc[df_caros]
 df_1M
-
 #selecionar colunas cidade, bairro e valor
 df2 = df[['Cidade', 'Bairro', 'Valor_Imovel']]
 df2.head()
-
 #filtrar imóveis de Curitiba
 df_curitiba = df[df['Cidade'] == 'Curitiba']
 df_curitiba
-
 #verificar valores nulos
 df.isnull().sum()
-
 #ordenar 10 imóveis mais caros
 df.sort_values(by='Valor_Imovel', ascending=False).head(10)
-
 #valor médio
 df['Valor_Imovel'].mean()
-
 #valor mediano
 df['Valor_Imovel'].median()
-
 #desvio padrão
 print(df['Valor_Imovel'].std())
-
 #valor mínimo e máximo da área construída
 df['Area_m2'].min()
 df['Area_m2'].max()
-
 #número de imóveis estão abaixo/acima da média
 media = df['Valor_Imovel'].mean()
 "Abaixo:", (df['Valor_Imovel'] < media).sum()
@@ -69,11 +53,9 @@ len(df_menor)
 filtro2 = df["Valor_Imovel"] > media
 df_maior = df.loc[filtro2]
 len(df_maior)
-
 #inserir coluna valor_m2
 df['valor_m2'] = df['Valor_Imovel'] / df['Area_m2']
 df.head()
-
 #inserir linha 
 nova_linha = {
     'ID_Imovel': 9999,
@@ -92,15 +74,11 @@ df = pd.concat([df, pd.DataFrame([nova_linha])], ignore_index=True)
 
 #valores nulos
 df.isnull().sum()
-
 #remover imóveis com Numero_Quartos = 5
 df = df[df['Numero_Quartos'] == 5]
-
 #excluir coluna ID_Imovel
 df = df.drop(columns=['ID_Imovel'])
-
 #remover imóveis da cidade "Teste"
 df = df[df['Cidade'] == 'Teste']
-
 #agrupar por cidade e calcular média de valor dos imóveis
 df.groupby('Cidade')['Valor_Imovel'].mean()

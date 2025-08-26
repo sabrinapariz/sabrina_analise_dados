c24bac747111f6dccdc97aacf375ac2552ac1c10
Angelo Pariz <parizsabrina@gmail.com>
2025-08-26 09:10:35 -0300

    aula5_regex.py

---
+++ aula5_regex.py
@@ -0,0 +1,28 @@
+import pandas as pd
+file = "C:\\Users\\angel\\OneDrive\\Documentos\\analise_de_dados\\cadastro_alunos.xlsx"
+df = pd.read_excel(file)
+ #case false traz tanto maiúsculo tanto minúsculo
+ # ($) final (^) começo (.) qualquer caracter (*) traz todos que possuem as letras
+filtro = df["nome_aluno"].str.contains("^sabrina|ana|lucas", case=False)
+df.loc[filtro]
+
+import requests
+import pandas as pd
+url = "http://www.ipeadata.gov.br/api/odata4/Metadados"
+response = requests.get(url)
+metadados = response.json()
+metadados = metadados["value"]
+df = pd.DataFrame(metadados)
+filtro = df["SERNOME"].str.contains("IPCA - educação, leitura e papelaria - taxa de variação", case=False)
+df.loc[filtro, ["SERNOME"]].values
+df.loc[filtro]
+
+#Acessar o código do IBGE
+SERCODIGO = "PRECOS12_IPCAED12"
+url = f"http://www.ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='{SERCODIGO}')"
+response = requests.get(url)
+dados = response.json()
+dados = dados["value"]
+df = pd.DataFrame(dados)
+df["VALDATA"] = pd.to_datetime(df["VALDATA"], errors="coerce")
+df[["VALDATA", "VALVALOR"]].plot()
